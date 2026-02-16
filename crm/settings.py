0c5ad8cd01a5da61110946909585d7c195391781
karim byd <zrika9324@gmail.com>
2026-02-16 13:45:05 +0000

    settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from celery.schedules import crontab
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -15,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_crontab',
+    'django_celery_beat',
 ]
 """"""
 CRONJOBS = [
@@ -22,6 +24,14 @@ CRONJOBS = [
     ('0 */12 * * *', 'crm.cron.update_low_stock'),
 ]
 """"""
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
 
 DATABASES = {
     'default': {
